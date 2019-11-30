a86f3b60ade7e2719ad08d7e94b11a7cecd9967b
gmemmy <gmemmy22@gmail.com>
2019-11-30 19:56:55 +0100

    setup eslint

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Feed from './src/screens/feed/';
+
+export default function App() {
+  return (
+    <Feed />
+  );
+}
---
+++ src/screens/feed/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+const Feed = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
+    </View>
+  )
+}
+
+export default Feed;
