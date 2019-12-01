107a55e384a9576bc7a46e320b060229942e2fde
gmemmy <gmemmy22@gmail.com>
2019-12-01 03:06:36 +0100

    finish post design

--- src/screens/feed/index.js
+++ src/screens/feed/index.js
@@ -1,11 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import { View } from 'react-native';
+import {
+  View, Text, Image, TouchableOpacity
+} from 'react-native';
+import { Ionicons, Entypo } from '@expo/vector-icons';
 import styles from './styles';
 import globalStyles from '../../utils/globalStyles';
 
 // Components
 import Header from '../../components/header';
 import PostHeader from '../../components/postHeader';
+
+// Images
+const post = require('../../../assets/profile.jpg');
 
 const Feed = () => (
   <View style={styles.container}>
@@ -14,6 +21,32 @@ const Feed = () => (
     </View>
     <View style={styles.postHeader}>
       <PostHeader />
+    </View>
+    <TouchableOpacity style={styles.post}>
+      <Image
+        source={post}
+        style={styles.postImage}
+      />
+    </TouchableOpacity>
+    <View style={styles.postBottom}>
+      <TouchableOpacity>
+        <Ionicons
+          name="ios-heart-empty"
+          color="#D2895C"
+          size={25}
+        />
+      </TouchableOpacity>
+      <Text style={styles.likeCount}>
+        418 likes
+      </Text>
+      <TouchableOpacity>
+        <Entypo
+          name="dots-three-vertical"
+          size={25}
+          color="white"
+          style={styles.threeDotsIcon}
+        />
+      </TouchableOpacity>
     </View>
   </View>
 );
--- src/screens/feed/styles.js
+++ src/screens/feed/styles.js
@@ -1,6 +1,7 @@
 import { StyleSheet } from 'react-native';
 import {
-  heightPercentageToDP as hp
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp
 } from 'react-native-responsive-screen';
 
 const styles = StyleSheet.create({
@@ -12,6 +13,32 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: hp(2),
+  },
+  post: {
+    overflow: 'hidden',
+    maxHeight: hp(31),
+    maxWidth: wp(100),
+    marginTop: hp(2),
+  },
+  postImage: {
+    height: wp(100),
+    width: wp(100),
+    marginLeft: wp(5),
+    borderRadius: wp(15) / 2,
+  },
+  postBottom: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: hp(2),
+    marginLeft: wp(4)
+  },
+  likeCount: {
+    marginLeft: wp(4),
+    color: '#ffff',
+    fontWeight: 'bold'
+  },
+  threeDotsIcon: {
+    marginLeft: wp(60)
   }
 });
 
